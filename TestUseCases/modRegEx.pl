b670a31d764b6c6777fb209f6077f9c8b62214e5
kheidt <konstantin.heidt@gmail.com>
2022-12-19 18:25:12 +0100

    pre test

---
+++ TestUseCases/modRegEx.pl
@@ -0,0 +1,8 @@
+INIT{undef $/;}
+
+# Profile ModRegEx
+s|(?s)((?<one>\n\s*<objectPermissions>(?:(?!<objectPermissions>).)*?<object>)(?<SEL>Planet__c)(?<three></object>.*?<viewAllRecords>)(?<MOD>.*?)(?<five></viewAllRecords>(?:(?!<objectPermissions>).)*?</objectPermissions>))|$+{one}$+{SEL}$+{three}true$+{five}|gs;
+s|(?s)((?<one>\n\s*<applicationVisibilities>(?:(?!<applicationVisibilities>).)*?<application>)(?<SEL>MyApplication)(?<three></application>.*?<visible>)(?<MOD>.*?)(?<five></visible>(?:(?!<applicationVisibilities>).)*?</applicationVisibilities>))|$+{one}$+{SEL}$+{three}true$+{five}|gs;
+
+# Labels ModRegEx
+s|(?s)((?<one>\n\s*<labels>(?:(?!<labels>).)*?<fullName>)(?<SEL>TestCustomLabel)(?<three></fullName>.*?<value>)(?<MOD>.*?)(?<five></value>(?:(?!<labels>).)*?</labels>))|$+{one}$+{SEL}$+{three}RunId_updateCommit_selected+{five}|gs;
